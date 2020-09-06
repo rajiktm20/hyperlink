1dd777da87aaddbc1fe36de09545fcc3c89e8442
rajiktm20 <rajiktm2014@gmail.com>
2020-09-06 21:25:37 +0530

    initial commit

---
+++ src/test/java/org/Login/Clone.java
@@ -0,0 +1,15 @@
+package org.Login;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Clone {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.Chrome.driver", "C:\\Users\\Public\\Selenium\\Driver\\chromedriver.exe");
+		
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.snapdeal.com");
+	
+
+	}
+}
